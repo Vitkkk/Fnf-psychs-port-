372f13ff5d61f0d072d5668af8f171427b620ef5
Vitkkk <asstecbp@gmail.com>
2026-08-15 10:22:09 -0300

    fix(events): make event field a real module type

--- overlay/source/editors/mobile/MobileEventField.hx
+++ overlay/source/editors/mobile/MobileEventField.hx
@@ -1,3 +1,12 @@
 package editors.mobile;
 
-typedef MobileEventField = editors.mobile.MobileEventRegistry.MobileEventField;
+typedef MobileEventField =
+{
+    var id:String;
+    var label:String;
+    var type:String;
+    var defaultValue:String;
+    @:optional var options:Array<String>;
+    @:optional var min:Float;
+    @:optional var max:Float;
+}
